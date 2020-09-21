2b9b4cd1bd3d02c4c1e9fd8e2494225df9535e6c
Remigiusz Oleszczuk <remy@boclips.com>
2020-09-21 10:14:10 +0200

    Add storybook for search-bar
    [#174837350]

--- lib/components/search-bar/src/index.stories.tsx
+++ lib/components/search-bar/src/index.stories.tsx
@@ -1,29 +1,21 @@
-import React from 'react';
-import { Story, Meta } from '@storybook/react/types-6-0';
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
 
-import SearchBar, { Props } from './index';
+import SearchBar, { Props } from "./index";
 
 export default {
-  title: 'SearchBar',
+  title: "SearchBar",
   component: SearchBar,
 } as Meta;
 
-const styles = {
-  display: 'flex',
-  justifyContent: 'center',
-};
-
-// eslint-disable-next-line react/jsx-props-no-spreading
-const Template: Story<Props> = (args) => (
-  <div style={styles}>
-    <SearchBar {...args} />
-  </div>
+const Template: Story<Props> = ({ onSearch, placeholder, theme }: Props) => (
+  <SearchBar onSearch={onSearch} placeholder={placeholder} theme={theme} />
 );
 
-export const Primary = Template.bind({});
+export const LTI = Template.bind({});
 
-Primary.args = {
-  onSearch: () => console.log('search'),
-  placeholder: 'Search...',
-  theme: 'lti',
+LTI.args = {
+  onSearch: () => console.log("search"),
+  placeholder: "Search...",
+  theme: "lti",
 };
