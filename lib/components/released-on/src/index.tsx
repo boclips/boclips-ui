a9f314e9822d2b00eb03fbba08de73bfb5c49187
Marcin Janik <marcin@boclips.com>
2020-09-23 13:41:45 +0200

    New ReleasedOn component
    Signed-off-by: Remigiusz Oleszczuk <remy@boclips.com>

---
+++ lib/components/released-on/src/index.tsx
@@ -0,0 +1,35 @@
+import React, { ReactElement } from "react";
+import c from "classnames";
+import s from "./style.module.less";
+
+export interface ReleasedOnProps {
+  releasedOn: Date;
+  createdBy: string;
+  theme?: string;
+}
+const ReleasedOn = ({
+  releasedOn,
+  createdBy,
+  theme = "teachers",
+}: ReleasedOnProps): ReactElement => {
+  const date = new Date(releasedOn);
+  const month = date.toLocaleDateString("en-Gb", { month: "short" });
+  const year = date.toLocaleDateString("en-Gb", { year: "numeric" });
+  const day = date.toLocaleDateString("en-Gb", { day: "2-digit" });
+  return (
+    <div
+      className={c(s.releasedOn, {
+        [s.lti]: theme === "lti",
+        [s.backoffice]: theme === "backoffice",
+        [s.custom]: theme === "custom",
+      })}
+    >
+      <span className={s.releasedOnDate}>
+        Released on {month} {day}, {year}
+      </span>
+      <span className={s.releasedOnBy}> by {createdBy} </span>
+    </div>
+  );
+};
+
+export default ReleasedOn;
