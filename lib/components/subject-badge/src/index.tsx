74e1f171882211de7b0f8e1f1636265c428669d7
Alex Glen <alex@boclips.com>
2020-09-23 16:36:07 +0100

    Add subject badge
    [#174875912]

---
+++ lib/components/subject-badge/src/index.tsx
@@ -0,0 +1,23 @@
+import React, { ReactElement } from "react";
+import Badge from "@boclips-ui/badge";
+
+interface Subject {
+  id: string;
+  name: string;
+}
+
+interface SubjectBadgeProps {
+  subject: Subject;
+  theme?: "backoffice" | "lti" | "custom";
+}
+
+const SubjectBadge = ({
+  subject,
+  theme = "lti",
+}: SubjectBadgeProps): ReactElement => (
+  <div data-qa="subject-badge">
+    <Badge value={subject.name} label="Subject:" theme={theme} />
+  </div>
+);
+
+export default SubjectBadge;
