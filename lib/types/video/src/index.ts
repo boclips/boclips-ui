2f9cdbdae58a677ae1ddc1bb35e871520f67c3e7
Marcin Janik <marcin@boclips.com>
2020-09-24 13:36:08 +0200

    Add type video
    [#174875912]

---
+++ lib/types/video/src/index.ts
@@ -0,0 +1,43 @@
+import URI from "urijs";
+import "urijs/src/URITemplate";
+import { Link as ApiLink } from "boclips-api-client/dist/types";
+
+export class Link {
+  private link: RawLink;
+
+  public constructor(link: RawLink) {
+    this.link = link;
+  }
+
+  public getOriginalLink() {
+    return this.link.href;
+  }
+
+  public get isTemplated(): boolean {
+    return this.link.templated === true;
+  }
+
+  public getTemplatedLink(paramKeysValues: {
+    [paramName: string]: any;
+  }): string {
+    if (URI.expand) {
+      return URI.expand(this.link.href, paramKeysValues).toString();
+    }
+    return "";
+  }
+}
+
+export interface RawLink {
+  href: string;
+  templated?: boolean;
+}
+
+export const convertToApiClientLink = (link?: Link): ApiLink | undefined =>
+  link
+    ? new ApiLink({ href: link.getOriginalLink(), templated: link.isTemplated })
+    : undefined;
+
+export const convertFromApiClientLink = (link?: ApiLink): Link | undefined =>
+  link
+    ? new Link({ href: link.getOriginalLink(), templated: link.isTemplated })
+    : undefined;
