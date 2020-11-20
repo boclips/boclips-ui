4aedbfb7f1b165705b19aaea6e5e36a5ae5833cb
Remigiusz Oleszczuk <remy@boclips.com>
2020-11-20 08:42:36 +0100

    Add styles for search bar clear icon
    [#175739402]

--- lib/components/video-card/storybook/index.stories.tsx
+++ lib/components/video-card/storybook/index.stories.tsx
@@ -51,64 +51,124 @@ const Template: Story<Props & Components> = ({
 );
 
 const video = {
-  id: "5d5c235c3eba390e95c1fa09",
-  title: "Knots on a Counting Rope read by Bonnie Bartlett & William Daniels",
+  id: "5c54d7cdd8eafeecae20ee58",
+  title: "TED-ED: Distorting Madonna in Medieval art - James Earle",
   description:
-    "Knots on a Counting Rope is written by Bill Martin Jr. and John Archambault, illustrated by Ted Rand and read by Bonnie Bartlett and William Daniels.\n\nThe counting rope is a metaphor for the passage of time and for a boy's emerging confidence facing his greatest challenges: blindness and the approaching death of his beloved grandfather.\n\nView the activity guide here: http://bit.ly/KnotsonaCountingRope\n\nWatch all of our videos at http://www.storylineonline.net/.",
-  releasedOn: "2012-05-21",
+    "After Rome was destroyed, people were wary of attachment to physical beauty. As Christianity gained traction, Romans instead began to focus on the metaphysical beauty of virtue, and art began to follow suit.  James Earle discusses how Medieval paintings of Madonna were affected by this shift.",
+  additionalDescription: null,
+  releasedOn: "2013-02-19",
   playback: {
-    type: "YOUTUBE",
-    id: "FgwmxMEiJ2U",
-    duration: "PT10M49S",
+    type: "STREAM",
+    type: "STREAM",
+    id: "0_0r24xtco",
+    duration: "PT3M7S",
+    referenceId: "2e4fbf0c-d0eb-4cdc-a6aa-534df1f15400",
+    maxResolutionAvailable: false,
     _links: {
       createPlaybackEvent: {
-        href: "https://api.staging-boclips.com/v1/events/playback",
+        href: "https://api.boclips.com/v1/events/playback",
         templated: false,
       },
       createPlayerInteractedWithEvent: {
-        href: "https://api.staging-boclips.com/v1/events/player-interaction",
+        href: "https://api.boclips.com/v1/events/player-interaction",
+        templated: false,
+      },
+      download: {
+        href:
+          "https://cdnapisec.kaltura.com/p/1776261/sp/177626100/playManifest/entryId/0_0r24xtco/format/download/protocol/https/flavorParamIds/0",
         templated: false,
       },
       thumbnail: {
-        href: "https://i.ytimg.com/vi/FgwmxMEiJ2U/hqdefault.jpg",
+        href:
+          "https://cdnapisec.kaltura.com/p/1776261/thumbnail/entry_id/0_0r24xtco/width/{thumbnailWidth}/vid_slices/3/vid_slice/1",
+        templated: true,
+      },
+      setThumbnailBySecond: {
+        href:
+          "https://api.boclips.com/v1/videos/5c54d7cdd8eafeecae20ee58/playback{?thumbnailSecond}",
+        templated: true,
+      },
+      setCustomThumbnail: {
+        href:
+          "https://api.boclips.com/v1/videos/5c54d7cdd8eafeecae20ee58/playback{?playbackId,thumbnailImage}",
+        templated: true,
+      },
+      videoPreview: {
+        href:
+          "https://cdnapisec.kaltura.com/p/1776261/thumbnail/entry_id/0_0r24xtco/width/{thumbnailWidth}/vid_slices/{thumbnailCount}",
+        templated: true,
+      },
+      hlsStream: {
+        href:
+          "https://cdnapisec.kaltura.com/p/1776261/sp/177626100/playManifest/entryId/0_0r24xtco/format/applehttp/flavorParamIds/487041%2C487051%2C487061%2C487071%2C487081%2C487091%2C487111%2C1049881/protocol/https/video.mp4",
         templated: false,
       },
     },
-    downloadUrl: null,
   },
-  subjects: [
-    { id: "5cb499c9fd5beb428189455d", name: "Fine Art" },
-    { id: "5cb499c9fd5beb428189454c", name: "2Arabic" },
-    { id: "5cb499c9fd5beb428189454f", name: "Design" },
-  ],
-  badges: ["youtube"],
-  legalRestrictions: "",
+  subjects: [{ id: "5cb1f6ba5c9cb675c59df37b", name: "Art History" }],
+  badges: ["ad-free"],
+  legalRestrictions:
+    "No editing permitted. Please contact boclips for full pricing and availability of TED-Ed videos.",
   ageRange: new AgeRange(5, 8),
   rating: null,
   yourRating: null,
-  createdBy: "StorylineOnline",
+  bestFor: [],
+  createdBy: "TED-Ed",
   promoted: null,
   language: null,
   attachments: [],
+  contentWarnings: [],
+  channel: "TED-Ed",
+  channelId: "5cf141cbc1475c47f717870d",
+  channelVideoId: "1212_06_A",
+  types: [{ id: 3, name: "Instructional Clips" }],
+  captionStatus: null,
+  isVoiced: null,
   _links: {
     self: {
-      href:
-        "https://api.staging-boclips.com/v1/videos/5d5c235c3eba390e95c1fa09",
+      href: "https://api.boclips.com/v1/videos/5c54d7cdd8eafeecae20ee58",
       templated: false,
     },
     logInteraction: {
       href:
-        "https://api.staging-boclips.com/v1/videos/5d5c235c3eba390e95c1fa09/events?logVideoInteraction=true&type={type}",
+        "https://api.boclips.com/v1/videos/5c54d7cdd8eafeecae20ee58/events?logVideoInteraction=true&type={type}",
       templated: true,
+    },
+    detailsProjection: {
+      href:
+        "https://api.boclips.com/v1/videos/5c54d7cdd8eafeecae20ee58?projection=details",
+      templated: false,
+    },
+    fullProjection: {
+      href:
+        "https://api.boclips.com/v1/videos/5c54d7cdd8eafeecae20ee58?projection=full",
+      templated: false,
+    },
+    assets: {
+      href: "https://api.boclips.com/v1/videos/5c54d7cdd8eafeecae20ee58/assets",
+      templated: false,
     },
     rate: {
       href:
-        "https://api.staging-boclips.com/v1/videos/5d5c235c3eba390e95c1fa09?rating={rating}",
+        "https://api.boclips.com/v1/videos/5c54d7cdd8eafeecae20ee58?rating={rating}",
       templated: true,
     },
-    tag: {
+    update: {
+      href: "https://api.boclips.com/v1/videos/5c54d7cdd8eafeecae20ee58",
+      templated: false,
+    },
+    addAttachment: {
       href:
-        "https://api.staging-boclips.com/v1/videos/5d5c235c3eba390e95c1fa09/tags",
+        "https://api.boclips.com/v1/videos/5c54d7cdd8eafeecae20ee58/attachments",
+      templated: false,
+    },
+    tag: {
+      href: "https://api.boclips.com/v1/videos/5c54d7cdd8eafeecae20ee58/tags",
+      templated: false,
+    },
+    transcript: {
+      href:
+        "https://api.boclips.com/v1/videos/5c54d7cdd8eafeecae20ee58/transcript",
       templated: false,
     },
   },
