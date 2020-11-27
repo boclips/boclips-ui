1846811321907d240110a863f37febadafbbf13f
Matt Jones <matt@boclips.com>
2020-11-27 17:01:47 +0000

    Started a second version
    - Now optionally passing in components rather than flags
    - Title and description now optional to remove edit mode
    
    Signed-off-by: Alex Glen <alex@boclips.com>

--- lib/components/best-for-badge/src/index.tsx
+++ lib/components/best-for-badge/src/index.tsx
@@ -5,6 +5,7 @@ export interface BestForBadgeProps {
   bestFor: string;
   theme?: "lti" | "hq" | "publishers";
 }
+
 const BestForBadge = ({ bestFor, theme }: BestForBadgeProps) => (
   <div data-qa="best-for-badge">
     <Badge value={bestFor} label="Best for:" theme={theme} />
