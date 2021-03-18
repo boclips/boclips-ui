0ae1768e369ab4a0edb5899f82c9f19cc0aab7c8
Matt Jones <matt@boclips.com>
2021-03-18 17:32:33 +0000

    Add antd styles import for release-date-filter
    [#177367827]
    
    Signed-off-by: Mima Farley-Rose <mima@boclips.com>

--- lib/components/release-date-filter/src/datePicker.tsx
+++ lib/components/release-date-filter/src/datePicker.tsx
@@ -1,6 +1,7 @@
 import { Dayjs } from "dayjs";
 import dayjsGenerateConfig from "rc-picker/lib/generate/dayjs";
 import generatePicker from "antd/lib/date-picker/generatePicker";
+import "antd/es/date-picker/style/index";
 
 const DatePicker = generatePicker<Dayjs>(dayjsGenerateConfig);
 
