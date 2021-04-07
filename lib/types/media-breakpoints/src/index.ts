8f43b51d1776a3eff5cffbb2da83ad3fcc6266f3
Remigiusz Oleszczuk <remy@boclips.com>
2021-04-07 09:48:08 +0200

    Add type to breakpoints:  mobile/tablet/desktop
    [#177565690]

--- lib/types/media-breakpoints/src/index.ts
+++ lib/types/media-breakpoints/src/index.ts
@@ -3,15 +3,16 @@ export declare type AntBreakpoint = "xxl" | "xl" | "lg" | "md" | "sm";
 export interface Breakpoint {
   width: number;
   label: AntBreakpoint;
+  type: string;
 }
 type Breakpoints = { [name in AntBreakpoint]: Breakpoint };
 
 export const MediaBreakpoints: Breakpoints = {
-  sm: { width: 320, label: "sm" },
-  md: { width: 768, label: "md" },
-  lg: { width: 1148, label: "lg" },
-  xl: { width: 1680, label: "xl" },
-  xxl: { width: 1920, label: "xxl" },
+  sm: { width: 320, label: "sm", type: "mobile" },
+  md: { width: 768, label: "md", type: "tablet" },
+  lg: { width: 1148, label: "lg", type: "desktop" },
+  xl: { width: 1680, label: "xl", type: "desktop" },
+  xxl: { width: 1920, label: "xxl", type: "desktop" },
 };
 
 export default MediaBreakpoints;
