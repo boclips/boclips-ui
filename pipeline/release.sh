686f8f810c24b319aa2e2c14763f866674de7a93
Eva Varga <eva@boclips.com>
2021-09-08 17:36:00 +0100

    Add logging to release script
    Signed-off-by: Andrew Bruce <me@andrewbruce.net>

--- pipeline/release.sh
+++ pipeline/release.sh
@@ -6,8 +6,12 @@ set -e
 cd source
 
 echo //registry.npmjs.org/:_authToken=${NPM_TOKEN} > .npmrc
+echo "about to run npm ci"
 npm ci
+echo "about to bootstrap components"
 npx lerna bootstrap
+echo "about to build components"
 npx lerna run build
+echo "about to publish changed components"
 npx lerna publish from-git --yes --no-git-tag-version --no-push
 )
