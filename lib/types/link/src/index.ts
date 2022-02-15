c8153e4ae7740349210613697d039d83aa5affa9
Eva Varga <eva@boclips.com>
2022-02-15 13:24:55 +0000

    Trigger new release for Link type

--- lib/types/link/src/index.ts
+++ lib/types/link/src/index.ts
@@ -3,7 +3,7 @@ import "urijs/src/URITemplate";
 import { Link as ApiLink } from "boclips-api-client/dist/types";
 
 /**
- * @deprecated Use `Link` type from `boclips-api-client` instead.
+ * @deprecated Use `Link` type from `boclips-api-client` instead
  */
 export class Link {
   private link: RawLink;
