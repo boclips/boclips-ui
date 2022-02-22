0129f24033aef2d56cdfcf39718db7b5d4b20448
Harsha Chaudhary <harsha@boclips.com>
2022-02-22 15:45:06 +0000

    Button can forward a ref
    [#180839314]
    
    Signed-off-by: Alex Glen <alex@boclips.com>

--- lib/components/button/src/index.tsx
+++ lib/components/button/src/index.tsx
@@ -16,40 +16,46 @@ export interface Props extends React.HTMLProps<HTMLButtonElement> {
   ariaLabel?: string;
 }
 
-const Button = ({
-  onClick,
-  iconOnly,
-  type,
-  disabled = false,
-  height,
-  width,
-  icon,
-  text,
-  dataQa,
-  suffix,
-  ariaLabel,
-  ...rest
-}: Props) => (
-  <button
-    aria-label={ariaLabel}
-    data-qa={dataQa}
-    onClick={onClick}
-    style={{ height, width }}
-    disabled={disabled}
-    className={c(s.button, {
-      [s.outline]: type === "outline",
-      [s.iconOnly]: iconOnly,
-    })}
-    type="button"
-    /* eslint-disable-next-line react/jsx-props-no-spreading */
-    {...rest}
-  >
-    {icon && <div className={s.icon}>{icon}</div>}
-    {!iconOnly && (
-      <span className={c(s.copy, { [s.copyWithIcon]: icon })}>{text}</span>
-    )}
-    {suffix && suffix}
-  </button>
+const Button = React.forwardRef(
+  (
+    {
+      onClick,
+      iconOnly,
+      type,
+      disabled = false,
+      height,
+      width,
+      icon,
+      text,
+      dataQa,
+      suffix,
+      ariaLabel,
+      ...rest
+    }: Props,
+    ref: React.Ref<HTMLButtonElement>
+  ) => (
+    <button
+      aria-label={ariaLabel}
+      data-qa={dataQa}
+      onClick={onClick}
+      style={{ height, width }}
+      disabled={disabled}
+      ref={ref}
+      className={c(s.button, {
+        [s.outline]: type === "outline",
+        [s.iconOnly]: iconOnly,
+      })}
+      type="button"
+      /* eslint-disable-next-line react/jsx-props-no-spreading */
+      {...rest}
+    >
+      {icon && <div className={s.icon}>{icon}</div>}
+      {!iconOnly && (
+        <span className={c(s.copy, { [s.copyWithIcon]: icon })}>{text}</span>
+      )}
+      {suffix && suffix}
+    </button>
+  )
 );
 
 export default Button;
